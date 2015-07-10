7832b494343828ebeb70b166ed18f6698986eee5
whyvic13 <haoyun_wen@hotmail.com>
2015-07-10 22:40:54 +0800

    Update 37.cpp

--- Q37/37.cpp
+++ Q37/37.cpp
@@ -1,36 +1,42 @@
-void solveSudoku(vector<vector<char> > &board) {
-    solveSudoku(board, 0, 0);
-}
-//backtrace
-bool solveSudoku(vector<vector<char> > &board, int row, int col) {
-    if (board.size()==row) return true;
-    if (board[row][col]!='.') {
-        if (col==board.size()-1) return solveSudoku(board, row+1, 0);
-        else return solveSudoku(board, row, col+1);
+class Solution {
+public:
+    void solveSudoku(vector<vector<char> > &board) {
+        solve(board);
     }
-    for (int i=1; i<=9; i++) {
-        if (check(board, row, col, '0'+i)) {
-            board[row][col] = '0'+i;
-            if (col==board.size()-1 && solveSudoku(board, row+1, 0)) return true;
-            else if (solveSudoku(board, row, col+1)) return true;
-            board[row][col] = '.';
+    //backtrace
+    bool solve(vector<vector<char> > &board) {
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
+                    for(char c='1';c<='9';c++){
+                        if(check(board,i,j,c)){
+                            board[i][j] = c;
+                            if(solve(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;//be care
+                }
+            }
         }
+        return true;//be care
     }
-    return false;
-}
 
-bool check(vector<vector<char> > &board, int row, int col, char value) {
-    int i;
-    for (i=0; i<9; i++) {
-        if (board[row][i]==value) return false;
+    bool check(vector<vector<char> > &board, int row,int col,char value) {
+        int i;
+        for (i=0; i<9; i++) {
+            if (board[row][i]==value) return false;
+        }
+        for (i=0; i<9; i++) {
+            if (board[i][col]==value) return false;
+        }
+        //Check 3 x 3 block
+        for(i = (row / 3) * 3; i < (row / 3) * 3 + 3; i++)
+            for(int j = (col / 3) * 3; j < (col / 3) * 3 + 3; j++)
+                if(board[i][j] == value)
+                    return false;
+        return true;
     }
-    for (i=0; i<9; i++) {
-        if (board[i][col]==value) return false;
-    }
-    for (i=0; i<9; i++) {
-        int r = (row/3)*3+i/3;
-        int c = (col/3)*3+i%3;
-        if (board[r][c]==value) return false;
-    }
-    return true;
-}
+};
