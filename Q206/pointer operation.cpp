7b31eefe2b2d981b0a33f65d7ab6c8a4de8139be
whyvic13 <haoyun_wen@hotmail.com>
2015-07-11 14:27:52 +0800

    Create pointer operation.cpp

---
+++ Q206/pointer operation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        while (head) {
+            ListNode* next = head -> next;
+            head -> next = pre;
+            pre = head;
+            head = next;
+        } 
+        return pre;
+    }
+};
