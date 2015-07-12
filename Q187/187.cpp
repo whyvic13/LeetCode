8c779771007bd9ca5bc923773411bdeb0eb1c76d
whyvic13 <haoyun_wen@hotmail.com>
2015-07-12 22:19:08 +0800

    Create 187.cpp

---
+++ Q187/187.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<size_t,int> map;//size_t,	The type of the hash values produced.
+        hash<string> fn;
+        vector<string> res;
+        for(int i=0;i<int(s.size())-9;i++){//if without int(), when s="", runtime error
+            if(map[fn(s.substr(i,10))]++ == 1){
+                res.push_back(s.substr(i,10));
+            }
+        }
+        return res;
+    }
+};
